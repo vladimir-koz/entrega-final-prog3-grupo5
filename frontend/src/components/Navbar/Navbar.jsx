43c0890058cd2a493825bc3f995aa70c5a57c8ba
santino <santttaloisio1196@gmail.com>
2026-07-05 19:31:35 -0300

    Empece diseño de frontend con el dashboard inicial y rutinas

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <header className="navbar">
+
+      <h2>PowerUp</h2>
+
+      <button>
+        Iniciar sesión
+      </button>
+
+    </header>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/pages/Progreso.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout/Layout";
+
+function Progreso() {
+    return (
+        <Layout>
+            <h1>Progreso</h1>
+        </Layout>
+    )
+}
+
+export default Progreso;
